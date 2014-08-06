e6e101fac6e758001658c3a59fdfee4b1360bcf7
Jonas Grau <jonas.grau@gmail.com>
2014-08-06 10:41:18 +0200

    remove dir setting

--- modules/projects/manifests/gomore.pp
+++ modules/projects/manifests/gomore.pp
@@ -2,7 +2,6 @@ class projects::gomore {
   include phantomjs
 
   boxen::project { 'gomore':
-    dir        => 'gomore-web',
     ruby       => '2.0.0-p353',
     postgresql => 'gomore',
     nginx      => true,
