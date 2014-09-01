eb7e3a920ba077cc09027d71c76f882fb3ff5758
Jonas Grau <jonas.grau@gmail.com>
2014-09-01 09:22:22 +0200

    set osx options

--- modules/people/manifests/jgrau/preferences.pp
+++ modules/people/manifests/jgrau/preferences.pp
@@ -4,10 +4,12 @@ class people::jgrau::preferences {
   include osx::global::expand_print_dialog
   include osx::global::expand_save_dialog
   include osx::global::disable_autocorrect
-  class { osx::global::key_repeat_rate: rate => 2 }
-  class { osx::global::key_repeat_delay: delay => 15 }
+  include osx::global::tap_to_click
+  class { 'osx::global::key_repeat_rate': rate => 2 }
+  class { 'osx::global::key_repeat_delay': delay => 10 }
   include osx::dock::autohide
   include osx::finder::show_hidden_files
   include osx::disable_app_quarantine
   include osx::no_network_dsstores
+  include osx::keyboard::capslock_to_control
 }
