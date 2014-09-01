c966e162d2b758fcac2f857ab8331fb1bece2824
Jonas Grau <jonas.grau@gmail.com>
2014-09-01 12:47:08 +0200

    gitgutter

--- modules/people/manifests/jgrau/vim.pp
+++ modules/people/manifests/jgrau/vim.pp
@@ -15,6 +15,7 @@ class people::jgrau::vim {
     'tpope/vim-endwise',
     'tpope/vim-dispatch',
     'tpope/vim-ragtag',
+    'airblade/vim-gitgutter',
     'gcmt/wildfire.vim',
     'vim-ruby/vim-ruby',
     'rodjek/vim-puppet',
