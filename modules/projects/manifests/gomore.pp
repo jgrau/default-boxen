970c25dfa3c1097d84ccf1aca1925a122737f95b
Jonas Grau <jonas.grau@gmail.com>
2014-10-17 13:53:54 +0200

    gomore uses elasticsearch

--- modules/projects/manifests/gomore.pp
+++ modules/projects/manifests/gomore.pp
@@ -2,9 +2,10 @@ class projects::gomore {
   include phantomjs
 
   boxen::project { 'gomore':
-    ruby       => '2.0.0-p353',
-    postgresql => 'gomore',
-    nginx      => true,
-    source     => 'gomore/gomore-web'
+    ruby          => '2.0.0-p353',
+    postgresql    => 'gomore',
+    nginx         => true,
+    elasticsearch => true,
+    source        => 'gomore/gomore-web'
   }
 }
