92a0235b48233da5e8beb4a256cf885eef94177e
Jonas Grau <jonas.grau@gmail.com>
2014-11-25 11:02:03 +0100

    more prefs

--- modules/people/manifests/jgrau/preferences.pp
+++ modules/people/manifests/jgrau/preferences.pp
@@ -4,6 +4,7 @@ class people::jgrau::preferences {
   include osx::global::expand_print_dialog
   include osx::global::expand_save_dialog
   include osx::global::disable_autocorrect
+  include osx::global::tap_to_click
   include osx::dock::autohide
   include osx::finder::show_hidden_files
   include osx::finder::empty_trash_securely
@@ -12,7 +13,12 @@ class people::jgrau::preferences {
   include osx::disable_app_quarantine
   include osx::no_network_dsstores
   include osx::keyboard::capslock_to_control
+  include osx::safari::enable_developer_mode
 
   class { 'osx::global::key_repeat_rate': rate => 2 }
   class { 'osx::global::key_repeat_delay': delay => 15 }
+
+  class { 'osx::global::natural_mouse_scrolling':
+      enabled => false
+  }
 }
